0a1e5ef374b880a694fd43c8078695aac0c4ef02
CodeBlue04 <colbyeaston@gmail.com>
2019-04-25 21:15:32 -0700

    Add files via upload

---
+++ Driver.py
@@ -0,0 +1,71 @@
+import time
+
+# -------------SatCom constants------------------
+# - need port number for communicator
+portID = 0
+# - need callback number for communicator
+callback = 0
+
+
+# -------- Satellite Communicator object----------
+# com = rockBlock.__init__(portID, callback)
+
+# ------function to send data from SensorArray to formatted string, then to the SatCom unit-------
+def sensorEmergency(data):
+    #- Message for crash with fire
+    if data[0] > 1 or data[1] > 1.5 or data[2] > 3 or data[3] > 181 or data[4] > 181 or \
+            data[5] > 181 and data[6] > 70:
+        # - 67 characters of text. Not enough room for data. When message hits server, modify it and fill in the
+        # text, regex for live testing because of 85 character message limit. Curretly 89 without timestamp.
+        # Probably a bool to indicate either manually generated or automatically generated messages
+        output = "AutoMsg: Crash with fire! Time: " + data[9] + " gX: " + "%.1f" % data[0] + " gY: " + \
+                 "%.1f" % data[1] + " gZ: " + "%.1f" % data[2] + " Lat: " + data[7] + \
+                 " Lon: " + data[8] + " Speed: " + data[10]
+        print(output)
+        # com.sendMessage(output)
+        return True
+
+    # - Message for a crash without fire
+    if data[0] > 1 or data[1] > 1.5 or data[2] > 3 or data[3] > 181 or data[4] > 181 or \
+            data[5] > 181 and data[6] < 70:
+        output = "AutoMsg: Crash! Time: " + data[9] + " gX: " + "%.1f" % data[0] + " gY: " + \
+                 "%.1f" % data[1] + " gZ: " + "%.1f" % data[2] + " Lat: " + data[7] + \
+                 " Lon: " + data[8] + " Speed: " + data[10]
+        print(output)
+        # com.sendMessage(output)
+        # - Message for fire alone
+        return True
+
+    # - Message for fire
+    elif data[6] > 70:
+        output = "AutoMsg: Fire! Time: " + +  data[9] + " Lat: " + "%.4f" % data[
+            7] + " Lon: " + "%.4f" % \
+                 data[8]
+        print(output)
+        # com.sendMessage(output)
+        return True
+
+
+#--------------------Driver script--------------------------
+def driverMonitor():
+    # com = rockBlock.__init__(rockBlock(0, 0)) - uncomment when SatCom is installed
+
+    # Python has an irritating import format
+    from SensorArray import SensorArray
+    sense = SensorArray()
+
+    #- Loop that dictates the behavior of the entire controller
+    while True:
+        sense.sensorCaller()
+        # if mes.listenForMessages(None):
+        # send message through com, log in output file
+
+        # if com.messageCheck():
+        # send message to mobile device, log to output file
+
+        #- Delay to make output more legible
+        time.sleep(0.03)
+
+
+#- Initiates primary function loop
+driverMonitor()
---
+++ IMUSensors.py
@@ -0,0 +1,120 @@
+import datetime
+import math
+from ctypes import c_short
+from time import sleep
+
+from smbus import SMBus
+
+import IMU
+import SensorArray
+
+
+class IMUSensors:
+    # Accelerometer:
+    IMU.detectIMU()  # Detect if BerryIMUv1 or BerryIMUv2 is connected.
+    IMU.initIMU()  # Initialise the accelerometer, gyroscope and compass
+
+    # Gyro
+    RAD_TO_DEG = 57.29578
+    M_PI = 3.14159265358979323846
+    G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
+    AA = 0.40  # Complementary filter constant
+
+    gyroXangle = 0.0
+    gyroYangle = 0.0
+    gyroZangle = 0.0
+
+    a = datetime.datetime.now()
+
+    # Thermometer:
+    addr = 0x77
+    oversampling = 3  # 0..3
+
+    bus = SMBus(1)  # 0 for R-Pi Rev. 1, 1 for Rev. 2
+
+    # return two bytes from data as a signed 16-bit value
+    def get_short(self, data, index):
+        return c_short((data[index] << 8) + data[index + 1]).value
+
+    # return two bytes from data as an unsigned 16-bit value
+    def get_ushort(self, data, index):
+        return (data[index] << 8) + data[index + 1]
+
+    # calls SensorArray class and passes array of sensor data if crash or fire detected in undateValues()
+    def sensorCaller(self, dataArray):
+        sense = SensorArray.SensorArray()
+        sense.alertDriver(dataArray)
+
+    #One function to rule them all. Or at least supply the bulk of sensor data.
+    def updateValues(self):
+        #--------------------Accelerometer sensor calculations-----------------------
+        # Read the accelerometer,gyroscope and magnetometer values
+        ACCx = IMU.readACCx()
+        ACCy = IMU.readACCy()
+        ACCz = IMU.readACCz()
+
+        xG = (ACCx * 0.244) / 1000
+        yG = (ACCy * 0.244) / 1000
+        zG = (ACCz * 0.244) / 1000
+
+        #-----------------Gyroscope sensor calculations-------------------------
+        GYRx = IMU.readGYRx()
+        GYRy = IMU.readGYRy()
+        GYRz = IMU.readGYRz()
+
+        ##Calculate loop Period(LP). How long between Gyro Reads
+        b = datetime.datetime.now() - self.a
+        self.a = datetime.datetime.now()
+        LP = b.microseconds / (1000000 * 1.0)
+
+        # Convert Gyro raw to degrees per second
+        rate_gyr_x = GYRx * self.G_GAIN
+        rate_gyr_y = GYRy * self.G_GAIN
+        rate_gyr_z = GYRz * self.G_GAIN
+
+        # Calculate the angles from the gyro.
+        self.gyroXangle += rate_gyr_x * LP
+        self.gyroYangle += rate_gyr_y * LP
+        self.gyroZangle += rate_gyr_z * LP
+
+        # Convert Accelerometer values to degrees
+        self.AccXangle = (math.atan2(ACCy, ACCz) * self.RAD_TO_DEG)
+        self.AccYangle = (math.atan2(ACCz, ACCx) + self.M_PI) * self.RAD_TO_DEG
+
+        # convert the values to -180 and +180
+        if self.AccYangle > 90:
+            self.AccYangle -= 270.0
+        else:
+            self.AccYangle += 90.0
+
+        #-------------------Thermometer calculations--------------------
+
+        # Read whole calibration EEPROM data
+        cal = self.bus.read_i2c_block_data(self.addr, 0xAA, 22)
+
+        # Convert byte data to word values
+        ac5 = self.get_ushort(cal, 8)
+        ac6 = self.get_ushort(cal, 10)
+        mc = self.get_short(cal, 18)
+        md = self.get_short(cal, 20)
+
+        self.bus.write_byte_data(self.addr, 0xF4, 0x2E)
+        sleep(0.005)
+        (msb, lsb) = self.bus.read_i2c_block_data(self.addr, 0xF6, 2)
+        ut = (msb << 8) + lsb
+
+        x1 = ((ut - ac6) * ac5) >> 15
+        x2 = (mc << 11) / (x1 + md)
+        b5 = x1 + x2
+        t = (b5 + 8) >> 4
+
+        temperature = t / 10.0
+
+        #-------------------Crash condition checker-----------------------
+        if temperature > 70 or self.gyroXangle > 181 or self.gyroYangle > 181 or self.gyroZangle > 181 or xG > 1 or yG \
+                > 1.5 or zG > 3:
+            #- creates list of data to send up to the SensorArray class
+            sensorData = [xG, yG, zG, self.gyroXangle, self.gyroYangle, self.gyroZangle, temperature]
+
+            #- Sends data to SensorArray
+            self.sensorCaller(sensorData)
